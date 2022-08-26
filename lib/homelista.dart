415f71db8816dd2da2899db570ece9eb7c265821
felipeioavasso <felipeioavasso@gmail.com>
2022-08-26 18:22:53 -0300

    first commit

---
+++ lib/homelista.dart
@@ -0,0 +1,191 @@
+import 'package:consumo_servico_avancado_revisao/post.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  dynamic _urlBase = "https://jsonplaceholder.typicode.com";
+
+  Future<List<Post>> _recuperarPostagens() async {
+    
+    http.Response response = await http.get( _urlBase+ "/posts" );
+    var dadosJson = json.decode( response.body );
+
+    List<Post> postagens = [];
+    for( var post in dadosJson ){
+      
+      print("post: " + post["title"] );
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add( p );
+    
+    }
+    return postagens;
+    //print( postagens.toString() );
+    
+  }
+
+  _post() async {
+
+    /* Post post = new Post(120, null, 'titulo', '_body');
+    var corpo = json.encode(
+      post.toJson()
+    ); */
+
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo",
+          "body": "Corpo da postagem"
+        }
+    );
+
+    http.Response response = await http.post(
+        _urlBase + "/posts",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: corpo
+    );
+
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+
+  }
+
+  _put() async {
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo",
+          "body": "Corpo da postagem"
+        }
+    );
+
+    http.Response response = await http.put(
+        _urlBase + "/posts/2",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: corpo
+    );
+
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _patch() async {
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo",
+          "body": "Corpo da postagem"
+        }
+    );
+
+    http.Response response = await http.patch(
+        _urlBase + "/posts/2",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: corpo
+    );
+
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _delete() async {
+    http.Response response = await http.delete(_urlBase + '/posts/2');
+
+    if ( response.statusCode == 200 ){
+      //Sucesso
+    }else{
+      //mensagem de erro
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Salvar"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Atualizar"),
+                  onPressed: _put,
+                ),
+                RaisedButton(
+                  child: Text("Remover"),
+                  onPressed: _post,
+                ),
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPostagens(),
+                builder: (context, snapshot){
+
+                  switch( snapshot.connectionState ){
+                    case ConnectionState.none :
+                    case ConnectionState.waiting :
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                      break;
+                    case ConnectionState.active :
+                    case ConnectionState.done :
+                      if( snapshot.hasError ){
+                        print("lista: Erro ao carregar ");
+                      }else {
+                        return ListView.builder(
+                            itemCount: snapshot.data?.length,
+                            itemBuilder: (context, index){
+
+                              List<Post>? lista = snapshot.data;
+                              Post post = lista![index];
+
+                              return ListTile(
+                                title: Text( post.title ),
+                                subtitle: Text( post.id.toString() ),
+                              );
+
+                            }
+                        );
+
+                      }
+                      break;
+                  }
+                  return Container();
+
+                },
+              ),
+            )
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
